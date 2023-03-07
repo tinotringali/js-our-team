cd2d85decc11227f48d4c2309273949201585dd7
tinotringali <tringali.tino94@gmail.com>
2023-03-07 22:43:38 +0100

    our team

---
+++ JS/main.js
@@ -0,0 +1,45 @@
+'use strict';
+
+function createMember(tagElement, className, nome, ruolo, foto) {
+    const members = document.createElement(tagElement);
+    members.classList.add(className);
+    members.innerText = nome;
+    members.innerText = ruolo;
+    members.innerText = foto;
+    return members;
+}
+
+const team = [
+
+    {
+        nome: 'Wayne Barnett',
+        ruolo: 'Founder & CEO',
+        foto: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        nome: 'Angela Caroll',
+        ruolo: 'Chief Editor',
+        foto: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        nome: 'Walter Gordon',
+        ruolo: 'Office Manager',
+        foto: 'walter-gordon-office-manager.jpg',
+    },
+    {
+        nome: 'Angela Lopez',
+        ruolo: 'Social Media Manager',
+        foto: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        nome: 'Scott Estrada',
+        ruolo: 'Developer',
+        foto: 'scott-estrada-developer.jpg',
+    },
+    {
+        nome: 'Barbara Ramos',
+        ruolo: 'Graphic Designer',
+        foto: 'barbara-ramos-graphic-designer.jpg',
+    },
+];
+console.log(team)
